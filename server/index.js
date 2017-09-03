def8c817ad5c8945eeeee0d57488ef8eb0312d47
Tyler Kilburn <TylerKilburn@gmail.com>
2017-09-03 10:38:14 -0400

    Using an env variable for port now, falls back to 3000.

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+
 app.use(express.static('dist'));
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!');
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
 });
